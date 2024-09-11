b14c2137c82dfb9d140a6ed63a14aba0cc1f8d57
Ivan I. Atanasov <i.atanasov77@gmail.com>
2024-09-11 06:10:30 +0000

    Pass the WebSocket Port as Argument.

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
-
+/**
+ * MANUAL: http://socketo.me/docs/hello-world
+ */
+error_reporting( E_ERROR | E_WARNING | E_PARSE );
 require dirname(__FILE__) . '/vendor/autoload.php';
 
 use Ratchet\Server\IoServer;
@@ -10,11 +13,16 @@ use Ratchet\ConnectionInterface;
 
 class WebSocket implements MessageComponentInterface
 {
+    /** @var \SplObjectStorage */
     protected $clients;
+    
+    /** @var string */
+    protected $logFile;
     
     public function __construct()
     {
-        $this->clients = new \SplObjectStorage;
+        $this->clients  = new \SplObjectStorage;
+        $this->logFile  = '/var/log/websocket.log';
     }
     
     public function onOpen( ConnectionInterface $conn )
@@ -22,19 +30,24 @@ class WebSocket implements MessageComponentInterface
         // Store the new connection to send messages to later
         $this->clients->attach( $conn );
         
-        echo "New connection! ({$conn->resourceId})\n";
+        $logData    = "New connection! ({$conn->resourceId})\n";
+        \file_put_contents( $this->logFile, $logData, FILE_APPEND | LOCK_EX );
     }
     
     public function onMessage( ConnectionInterface $from, $msg )
     {
-        $numRecv = count($this->clients) - 1;
-        echo sprintf('Connection %d sending message "%s" to %d other connection%s' . "\n"
-            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's');
+        /*
+        $numRecv = count( $this->clients ) - 1;
+        echo sprintf( 
+            'Connection %d sending message "%s" to %d other connection%s' . "\n"
+            , $from->resourceId, $msg, $numRecv, $numRecv == 1 ? '' : 's'
+        );
+        */
         
-        foreach ($this->clients as $client) {
-            if ($from !== $client) {
+        foreach ( $this->clients as $client ) {
+            if ( $from !== $client ) {
                 // The sender is not the receiver, send to each client connected
-                $client->send($msg);
+                $client->send( $msg );
             }
         }
     }
@@ -44,12 +57,14 @@ class WebSocket implements MessageComponentInterface
         // The connection is closed, remove it, as we can no longer send it messages
         $this->clients->detach( $conn );
         
-        echo "Connection {$conn->resourceId} has disconnected\n";
+        $logData    = "Connection {$conn->resourceId} has disconnected\n";
+        \file_put_contents( $this->logFile, $logData, FILE_APPEND | LOCK_EX );
     }
     
     public function onError( ConnectionInterface $conn, \Exception $e )
     {
-        echo "An error has occurred: {$e->getMessage()}\n";
+        $logData    = "An error has occurred: {$e->getMessage()}\n";
+        \file_put_contents( $this->logFile, $logData, FILE_APPEND | LOCK_EX );
         
         $conn->close();
     }
@@ -61,7 +76,7 @@ $server = IoServer::factory(
             new WebSocket() // <-- My class, ignore it
         )
     ),
-    8090
+    $argv[1]
 );
 
 $server->run();
